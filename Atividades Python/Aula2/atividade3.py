79c5e4a3916b6ddfc3ce95204eb4cce59fcc82c2
Nicolay Neves <nicolayneves7@gmail.com>
2026-06-19 11:05:06 -0300

    Add meter conversion script to centimeters and millimeters

---
+++ Atividades Python/Aula2/atividade3.py
@@ -0,0 +1,10 @@
+# Solicita um valor em metros
+metros = float (input("Digite um valor em metros: "))
+
+#Conversões 
+centimetros = metros * 100
+milimetros = metros * 1000
+
+#Exibe os resultados 
+print (f"Valor em centímetros: ", {centimetros}, "cm")
+print (f"Valor em milimetros: ", {milimetros}, "mm")
