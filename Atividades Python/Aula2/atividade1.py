51c1607269dd79baf3a078af8fa75ac3a36ef06e
Nicolay Neves <nicolayneves7@gmail.com>
2026-06-19 10:34:23 -0300

    Add files via upload

---
+++ Atividades Python/Aula2/atividade1.py
@@ -0,0 +1,6 @@
+nome_produto = str (input ("Digite o nome do produto: "))
+quantidade_estoque= int (input("Digite o quantidade do estoque: "))
+preco_unitario = float (input("Digite o valor: "))
+
+# Exibir as informações
+print (f'o produto é: {nome_produto} quantidade de estoque é: {quantidade_estoque} e o valor é: {preco_unitario}')
---
+++ Atividades Python/Aula2/atividade2.py
@@ -0,0 +1,11 @@
+# Receber as quatros notas de aluno
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+nota4 = float(input("Digite a quarta nota: "))
+
+# Calcular a média aritmética simples
+media = (nota1 + nota2 + nota3 + nota4)/ 4
+
+# Mostrar resultado
+print (f'A média final do aluno é: {media}')
